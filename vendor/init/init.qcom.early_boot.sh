8a61e028eaa80c8aecab22f3017e2e7b86a1f825
Manikanta Kanamarlapudi <quic_kmanikan@quicinc.com>
2022-01-22 00:29:09 +0530

    vendor: init: common: Remove media netflix prop
    Remove media netflix prop for bengal variants.
    This is moved to video project.
    
    Change-Id: I8dddd001d8d91431d254a00e6e013e0ea0922ac5
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- vendor/init/init.qcom.early_boot.sh
+++ vendor/init/init.qcom.early_boot.sh
@@ -367,9 +367,7 @@ case "$target" in
                 setprop vendor.display.enhance_idle_time 1
                 setprop vendor.netflix.bsp_rev ""
                 ;;
-            *)
-                # default case is for bengal
-                setprop vendor.netflix.bsp_rev "Q6115-31409-1"
+             518)
                 ;;
         esac
         ;;
